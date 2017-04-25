66e744fb0b241ae10e82b0a43764a1f3f76d0de6
Randell Carrido <carridr1@tcnj.edu>
2017-04-24 23:15:46 -0400

    Code and Image commit
    Added more code to arduino, more interface design in iOS, more images

--- Arduino/mailDirect_Arduino/mailDirect_Arduino.ino
+++ Arduino/mailDirect_Arduino/mailDirect_Arduino.ino
@@ -1,11 +1,36 @@
 
-import WiFi;
-import Wire;
+
+const int trigPin = 9;
+const int echoPin = 10;
+
+long duration;
+int distance;
+
 void setup() {
-  bool mailSignal;
+  pinMode(trigPin,OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin,LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(trigPin,HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin,LOW);
+
+  duration = pulseIn(echoPin,HIGH);
+  distance = duration*0.034/2;
+
+  
+
+  if(distance != 11){
+    Serial.println("You've got mail!");
+  }
+  else{
+    Serial.println("No Mail.");
+  }
+  
 
 }
